78d907cd56d1f6d61eb98d84aefb91c51efc7e06
Jeffser <jeffrysamuer@gmail.com>
2026-03-26 17:13:32 -0600

    also load more stuff in grid mode song view

--- src/widgets/pages/songs.py
+++ src/widgets/pages/songs.py
@@ -14,6 +14,7 @@ class SongsPage(Adw.NavigationPage):
     list_el = Gtk.Template.Child()
     wrapbox_el = Gtk.Template.Child()
     end_stack = Gtk.Template.Child()
+    scrolledwindow = Gtk.Template.Child()
     offset = 0
     searching = False
 
@@ -21,7 +22,7 @@ class SongsPage(Adw.NavigationPage):
         if len(list(self.list_el.list_el)) + len(list(self.wrapbox_el)) == 0:
             GLib.idle_add(self.on_search, self.search_entry)
 
-    def search(self):
+    def search(self, count=30):
         if self.searching:
             return
         self.searching = True
@@ -29,7 +30,7 @@ class SongsPage(Adw.NavigationPage):
         integration = get_current_integration()
         search_results = integration.search(
             query=query,
-            songCount=30,
+            songCount=count,
             songOffset=self.offset
         )
         for song_id in search_results.get('song'):
@@ -47,8 +48,8 @@ class SongsPage(Adw.NavigationPage):
                 button = SongSmallRow(song_id)
                 GLib.idle_add(self.wrapbox_el.append, button)
 
-        GLib.idle_add(self.end_stack.set_visible_child_name, 'end' if len(search_results.get('song')) < 30 else 'loading')
-        self.offset += 30
+        GLib.idle_add(self.end_stack.set_visible_child_name, 'end' if len(search_results.get('song')) < count else 'loading')
+        self.offset += count
         self.searching = False
         GLib.idle_add(self.update_visibility)
 
@@ -63,6 +64,16 @@ class SongsPage(Adw.NavigationPage):
     def scroll_edge_reached(self, scrolledwindow, pos):
         if pos == Gtk.PositionType.BOTTOM and self.end_stack.get_visible_child_name() == 'loading':
             threading.Thread(target=self.search).start()
+
+    @Gtk.Template.Callback()
+    def toggle_view_changed(self, toggle_group, ud):
+        def check_scrollbar():
+            va = self.scrolledwindow.get_vadjustment()
+            if va.get_upper() <= va.get_page_size():
+                threading.Thread(target=self.search, args=(60,)).start()
+
+        if toggle_group.get_active_name() == "grid":
+            GLib.timeout_add(1000, check_scrollbar)
 
     def update_visibility(self):
         for row in list(self.list_el.list_el) + list(self.wrapbox_el):
